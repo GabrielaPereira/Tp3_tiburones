9ca704d349d2a463781e5da49fadd1db915de034
GabrielaPereira <31809158+GabrielaPereira@users.noreply.github.com>
2017-11-04 18:05:24 -0300

    sdsfd

--- Tp3/src/logica/Jugador.java
+++ Tp3/src/logica/Jugador.java
@@ -2,6 +2,30 @@ package logica;
 
 public class Jugador {
 
+	String nombre;
+	int nivel;
+	String posicion;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getNivel() {
+		return nivel;
+	}
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	public String getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+	
+	
 	
 	
 	
