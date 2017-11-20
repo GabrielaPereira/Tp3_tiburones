48d3e72bca4ca2cd20528a82217b905c20103eca
Usuario <Usuario@Xroos>
2017-11-20 13:24:56 -0300

    gaby agregue cosas a tu clase pero no cambie nada de lo que hiciste ..

---
+++ Tp3/src/Threads/ThreadBarra.java
@@ -0,0 +1,27 @@
+package Threads;
+
+import interfaz.IngresoDatos;
+
+
+public class ThreadBarra extends Thread{
+
+	public void run(){
+
+		
+		for(int i=0;i<=100;i++){
+			
+			IngresoDatos.PbhayEquipo.setValue(i);
+			
+			try {
+				
+				Thread.sleep(30);
+				
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+				// TODO: handle exception
+			}
+			
+		}
+}
